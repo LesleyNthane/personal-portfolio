c833a12ee8a5f50a6bec10d89940fcfbcf94bc67
LesleyNthane <nthanelesley2001@gmail.com>
2024-07-09 18:49:17 +0200

    Add the Contact component

---
+++ src/components/Contact.js
@@ -0,0 +1,39 @@
+import { useState } from "react"
+import { Col, Container, Row } from "react-bootstrap";
+import contactImg from "../assets/img/contact-img.svg"
+
+export const Contact = () => {
+    const formInitialDetails = {
+        firstName: '',
+        lastName: '',
+        email: '',
+        phone: '',
+        message: ''
+    }
+
+    const [formDetails, setFormDetails] = useState(formInitialDetails);
+    const [buttonText, setButtonText] = useState('Send');
+    const [status, setStatus] = useState({});
+
+    return (
+        <section className="contact" id="connect">
+            <Container>
+                <Row className="align-items-center">
+                    <Col md={6}>
+                    <img src={contactImg} alt="Contact Us" />
+                    </Col>
+                    <Col md={6}>
+                        <h2>Get In Touch</h2>
+                        <form>
+                            <Row>
+                                <Col sm={6} className="px-1">
+                                   /** Stopped at 1h: 08m */
+                                </Col>
+                            </Row>
+                        </form>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    )
+}
