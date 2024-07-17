d3aabfd889c36574a57f68026d213995e9f98fc9
LesleyNthane <nthanelesley2001@gmail.com>
2024-07-17 21:04:00 +0200

    Add footer component and finish up with mailchimpform

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { NavBar } from './components/NavBar';
 import { Banner } from './components/Banner';
 import { Skills } from './components/Skills';
+import { Footer } from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Projects } from './components/Projects';
 
@@ -14,6 +15,7 @@ function App() {
       <Banner />
       <Skills />
       <Projects />
+      <Footer />
     </div>
   );
 }
--- src/components/MailchimpForm.js
+++ src/components/MailchimpForm.js
@@ -3,7 +3,7 @@ import { Newsletter } from "./Newsletter";
 
 //for mailchimp : npm i react-mailchimp-subscribe
 
-export const mailchimpFormb = () => {
+export const mailchimpForm = () => {
     const postUrl = `${process.env.REACT_APP_MAILCHIMP_URL}?u=${process.env.REACT_APP_MAILCHIMP_U}$id=${process.env.REACT_APP_MAILCHIMP_ID}`;
     
     return (
@@ -14,7 +14,7 @@ export const mailchimpFormb = () => {
                     <Newsletter 
                         status={status}
                         message={message}
-                        onValidated={formData => subscribe(formDate)}
+                        onValidated={formData => subscribe(formData)}
                     />
                 )}
             />
