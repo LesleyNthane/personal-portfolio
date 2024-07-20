38bfb3ffc3c30906a37c7b4900ea33ba561cfa48
LesleyNthane <nthanelesley2001@gmail.com>
2024-07-20 23:55:20 +0200

    add animation for the banner component

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
 import headerImage from "../assets/img/header-img.svg";
+import 'animate.css';
+import TrackVisibility from "react-on-screen";
 
 /* for icons install :npm install react-bootstrap-icons --save */
 export const Banner = () => {
@@ -46,12 +48,16 @@ export const Banner = () => {
         <section className="banner" id="home">
             <Container>
                 <Row className="align-item-center">
-                    <Col xs={12} md={6} xl={7}>
-                        <span className="tagline">Welcome to my Portfolio</span>
-                        <h1>{`Hi I'm Lesley Nthane `}<span className="wrap">{text}</span></h1>
-                        <p>Paragraph about My self</p>
-                        <button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size={25} /></button>
-
+                    <Col xs={12} md={6} xl={7}>  
+                        <TrackVisibility>
+                        {({ isVisible }) => 
+                            <div className={isVisible ? "animated__animated animate__fadeIn" : ""}>
+                                <span className="tagline">Welcome to my Portfolio</span>
+                                <h1>{`Hi I'm Lesley Nthane `}<span className="wrap">{text}</span></h1>
+                                <p>Paragraph about My self</p>
+                                <button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size={25} /></button>
+                            </div>}
+                        </TrackVisibility>
                     </Col>
                     <Col xs={12} md={6} xl={5}>
                         <img src={headerImage} alt="Header Iamage" />
